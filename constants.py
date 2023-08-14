6ea8be5c4b8047a01fa13c4d4b34bd374489dc40
kazzart <misailidi.a@gmail.com>
2023-08-14 22:58:32 +0300

    Added sctructure, columns, boxplots

---
+++ constants.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+GROUP_TO_ANALYZE = 1
+
+DATES = dict(
+    (
+        (0, datetime(2023, 2, 8)),
+        (1, datetime(2023, 2, 15)),
+        (2, datetime(2023, 2, 15)),
+        (3, datetime(2023, 2, 15)),
+        (4, datetime(2023, 2, 15)),
+        (5, datetime(2023, 2, 26)),
+        (6, datetime(2023, 2, 26)),
+        (7, datetime(2023, 3, 4)),
+        (8, datetime(2023, 3, 12)),
+        (9, datetime(2023, 3, 18)),
+        (10, datetime(2023, 4, 1)),
+    )
+)
